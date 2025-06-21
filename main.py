7fa557ce13d26f8c4ca220c394bd00a8c55eb436
Hoa Ngo <xuanhoa2772004@gmail.com>
2025-06-21 18:10:36 +0700

    update metrics for prometheus

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import os
-
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.middleware.cors import CORSMiddleware
-
 from routers import student as student_router
 from configs.database import Base, engine
 from routers import authentication as auth_router
 load_dotenv()
 FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN")
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = FastAPI()
+
+REQUEST_COUNTER = Counter(
+    "fastapi_request_count",
+    "Số lượng request",
+    ["method", "endpoint", "status_code"]
+)
+
+class MetricsMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
+        REQUEST_COUNTER.labels(
+            method=request.method,
+            endpoint=request.url.path,
+            status_code=response.status_code
+        ).inc()
+        return response
+
+app.add_middleware(MetricsMiddleware)
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 app.add_middleware(
     CORSMiddleware,
